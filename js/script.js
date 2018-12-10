f48138f103fa990925c1898ea7c3006eef91bc32
Flo <Flo@moringa.com>
2018-12-10 09:49:26 +0300

    Set variables in js script

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var score = 0; //Set score to 0 initially
+var total = 5; //This is the total number of questions in this quiz
+var point = 1; // This refers to points awarded per correct answer
+var highest = total * point; //This is the highest possible score which is the total multiplied by the points awarded
