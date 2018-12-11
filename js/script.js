08abff0ef3769ef82ee29eb4a49e899bb0532157
Flo <Flo@moringa.com>
2018-12-11 11:13:33 +0300

    Add function to enable scoring submitted answers

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-var score = 0; //Set score to 0 initially. This will be increased by 1 for every correct answer submitted
-//Initializer
-// function init () {
-//   //set correct answers
-//   sessionStorage.setItem('a1','c');//'a1' represents answer 1. So, answer 1 is 'c'
-//   sessionStorage.setItem('a2','a');//'a2' represents answer 2. So, answer 2 is 'a'
-//   sessionStorage.setItem('a3','b');//'a3' represents answer 3. So, answer 3 is 'b'
-//   sessionStorage.setItem('a4','b');//'a4' represents answer 4. So, answer 4 is 'b'
-//   sessionStorage.setItem('a5','b');//'a5' represents answer 5. So, answer 5 is 'b'
-// }
 
 $(document).ready(function() {
   //Hide all questions
@@ -22,48 +12,47 @@ $(document).ready(function() {
 
   $('.questionForm #submit1').click(function() {
     var answer1 = $("input:radio[name=q1]:checked").val();
+    if(answer1 == "c"){
+      score++;
+    }
     $('.questionForm#q2').show();
     $('.questionForm#q1').hide();
   event.preventDefault();
   });
  $('.questionForm #submit2').click(function() {
   var answer2 = $("input:radio[name=q2]:checked").val();
+  if(answer2 == "a"){
+    score++;
+  }
   $('.questionForm#q3').show();
   $('.questionForm#q2').hide();
   event.preventDefault();
  })
  $('.questionForm #submit3').click(function() {
   var answer3 = $("input:radio[name=q3]:checked").val();
+  if(answer3 == "b"){
+    score++;
+  }
   $('.questionForm#q4').show();
   $('.questionForm#q3').hide();
   event.preventDefault();
  })
  $('.questionForm #submit4').click(function() {
   var answer4 = $("input:radio[name=q4]:checked").val();
+  if(answer4 == "b"){
+    score++;
+  }
   $('.questionForm#q5').show();
   $('.questionForm#q4').hide();
   event.preventDefault();
  })
  $('.questionForm #submit5').click(function() {
   var answer5 = $("input:radio[name=q5]:checked").val();
+  if(answer5 == "b"){
+    score++;
+  }
   $('.questionForm#q5').hide();
+  $('#results').html('<h3>Your final score is: '+score+' out of 5</h3><a href="index.html">Take Quiz Again</a>');
   event.preventDefault();
  })
 });
-
-//Process the answers. This will take the submitted answers and compare them with the correct answers that we have
-function process(n) {
-  //Get input value
-  var submitted = $('input [name=q'+n+']:checked').val();
-  if(submitted==sessionStorage.getItem('a'+n+'')) {
-        score++;
-  }
-
-  if(n==total) {
-    $('#results').html('<h3>Your final score is: '+score+' out of 5</h3><a href="index.html">Take Quiz Again</a>');
-  }
-  return false;
-}
-
-//Add event listener
-window.addEventListener('load', init, false);//This will listen for the load event. When the load event happens, it'll run 'init', which will set the answer items
